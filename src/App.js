e4eff897030da7bbf789ab93ed6a35e642219f93
Calum <calum@paceup.com>
2020-07-06 11:08:09 -0700

    master

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        Hello World
+        <button onClick={() => themeToggler()}>Change Theme</button>
+      </StyledApp>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/themes.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const lightTheme = {
+  body: "#fff",
+  fontColor: "#000",
+};
+
+export const darkTheme = {
+  body: "#000",
+  fontColor: "#fff",
+};
+
+export const GlobalStyles = createGlobalStyle`
+
+	body {
+
+		background-color: ${(props) => props.theme.body};
+
+	}
+
+`;
